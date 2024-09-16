9c534ffb4bb419054ee3577d68951a56ad180807
Plusnin.DA <72695485+zloihach@users.noreply.github.com>
2024-09-16 19:45:22 +0300

    edit graph

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const endpoint = 'http://localhost:3000/weather';
+    const endpoint = 'http://46.19.66.189:3000/weather';
     fetchDataAndRenderCharts(endpoint);
     setInterval(() => {
         fetchDataAndRenderCharts(endpoint);
@@ -16,74 +16,82 @@ function fetchDataAndRenderCharts(endpoint) {
             const lightData = dataArray.map(data => data.light);
             const pressureData = dataArray.map(data => data.pressure);
 
-            renderChart('temperatureChart', 'Температура', '°C', labels, temperatureData, 'rgb(255, 99, 132)', [5, 5]);
-            renderChart('humidityChart', 'Влажность', '%', labels, humidityData, 'rgb(54, 162, 235)');
-            renderChart('lightChart', 'Освещенность', 'Люкс', labels, lightData, 'rgb(255, 206, 86)', [10, 5]);
-            renderChart('pressureChart', 'Давление', 'мм рт. ст.', labels, pressureData, 'rgb(75, 192, 192)');
+            updateChart('temperatureChart', 'Температура', '°C', labels, temperatureData, 'rgb(255, 99, 132)', [5, 5]);
+            updateChart('humidityChart', 'Влажность', '%', labels, humidityData, 'rgb(54, 162, 235)');
+            updateChart('lightChart', 'Освещенность', 'Люкс', labels, lightData, 'rgb(255, 206, 86)', [10, 5]);
+            updateChart('pressureChart', 'Давление', 'мм рт. ст.', labels, pressureData, 'rgb(75, 192, 192)');
         })
         .catch(error => {
             console.error('Ошибка при выполнении запроса к API:', error);
         });
 }
 
-function renderChart(canvasId, label, unit, labels, dataValues, color, borderDash = []) {
+function updateChart(canvasId, label, unit, labels, dataValues, color, borderDash = []) {
     const ctx = document.getElementById(canvasId).getContext('2d');
+    
     if (window.myCharts === undefined) {
         window.myCharts = {};
     }
+    
     if (window.myCharts[canvasId] !== undefined) {
-        window.myCharts[canvasId].destroy();
-    }
-    window.myCharts[canvasId] = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: label,
-                data: dataValues,
-                fill: false,
-                borderColor: color,
-                tension: 0.1,
-                borderDash: borderDash,
-                borderWidth: 2,
-                pointBackgroundColor: color,
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: color,
-                pointRadius: 5,
-                pointHoverRadius: 7,
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: false,
-                    title: {
-                        display: true,
-                        text: unit
+        const chart = window.myCharts[canvasId];
+        chart.data.labels = labels;
+        chart.data.datasets[0].data = dataValues;
+        chart.update(); 
+    } else {
+        window.myCharts[canvasId] = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: label,
+                    data: dataValues,
+                    fill: false,
+                    borderColor: color,
+                    tension: 0.1,
+                    borderDash: borderDash,
+                    borderWidth: 2,
+                    pointBackgroundColor: color,
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: color,
+                    pointRadius: 5,
+                    pointHoverRadius: 7,
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: false,
+                        reverse: true, // Разворачиваем ось Y
+                        title: {
+                            display: true,
+                            text: unit
+                        }
+                    },
+                    x: {
+                        reverse: true, // Разворачиваем ось X (время)
+                        title: {
+                            display: true,
+                            text: 'Время'
+                        }
                     }
                 },
-                x: {
-                    title: {
+                plugins: {
+                    legend: {
                         display: true,
-                        text: 'Время'
+                        position: 'top',
+                    }
+                },
+                layout: {
+                    padding: {
+                        top: 30,
+                        right: 20,
+                        bottom: 0,
+                        left: 0
                     }
                 }
-            },
-            plugins: {
-                legend: {
-                    display: true,
-                    position: 'top',
-                }
-            },
-            layout: {
-                padding: {
-                    top: 30,
-                    right: 20,
-                    bottom: 0,
-                    left: 0
-                }
             }
-        }
-    });
+        });
+    }
 }
